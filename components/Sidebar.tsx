04479e13f62c0c23d8733f037f83b2ac7e2dfb3d
chenyu <3277502228@qq.com>
2022-12-03 20:28:14 +0800

    update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import Link from "next/link";
-import GoogleLogin from "react-google-login";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
 import Discover from "./Discover";
